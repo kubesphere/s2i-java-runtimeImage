d5f97ab79941526072a398b9a2a73c6dd32ab2bf
runzexia <runzexia@yunify.com>
2019-08-05 18:05:26 +0800

    add test script & fix not fould bug
    Signed-off-by: runzexia <runzexia@yunify.com>

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,31 +1,43 @@
 pipeline {
   agent {
     node {
-      label 'base'
-    }
+      label 'maven'
 
   }
   stages {
     stage('build and tag image') {
       steps {
-        container('base') {
-          sh '''docker build -t kubespheredev/s2i-java8-runtime .
-docker tag kubespheredev/s2i-java8-runtime kubespheredev/s2i-java8-runtime:advanced-2.1.0-dev'''
+        container('maven') {
+          sh './test.sh'
         }
 
       }
     }
+     stage('docker tag') {
+          when{
+            branch 'master'
+          }
+          steps {
+            container('maven') {
+                sh '''docker tag s2i-java8-runtime kubespheredev/java-8-runtime
+                docker tag s2i-java11-runtime kubespheredev/java-11-runtime
+                docker tag tomcat85-java8-runtime kubespheredev/tomcat85-java8-runtime
+                docker tag tomcat85-java11-runtime kubespheredev/tomcat85-java11-runtime
+                '''
+            }
+          }
+     }
     stage('docker push') {
       when{
         branch 'master'
       }
       steps {
-        container('base') {
-          withCredentials([usernamePassword(passwordVariable : 'DOCKER_PASSWORD' ,usernameVariable : 'DOCKER_USERNAME' ,credentialsId : 'dockerhub-id' ,)]) {
-            sh 'echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin'
-
-          }
-            sh 'docker push kubespheredev/s2i-java8-runtime'
+        container('maven') {
+                sh '''docker push kubespheredev/java-8-runtime
+                docker push kubespheredev/java-11-runtime
+                docker push kubespheredev/tomcat85-java8-runtime
+                docker push kubespheredev/tomcat85-java11-runtime
+                '''
         }
       }
     }
---
+++ test.sh
@@ -0,0 +1,216 @@
+#!/bin/bash
+set -ex
+
+# ==================================================================================
+
+function test_app() {
+  local name=$1
+  local port="8080"
+
+  local container_id=$(docker run --name ${name}-test -d -p ${port} ${name})
+
+  # sleep is required because after docker run returns, the container is up but our server may not quite be yet
+  sleep 10
+
+  local http_port="$(docker port ${container_id} ${port}|sed 's/0.0.0.0://')"
+  local http_reply=$(curl --silent --show-error http://localhost:$http_port)
+
+  if [ "$http_reply" = 'hello, world' ]; then
+    echo "APP TEST PASSED"
+    docker rm -f ${container_id}
+    return 0
+  else
+    echo "APP TEST FAILED"
+    docker logs ${container_id}
+    docker rm -f ${container_id}
+    return -123
+  fi
+}
+
+# ==================================================================================
+
+function test_entrypoint() {
+  local name=$1
+  local entrypoint=$2
+
+  local container_id=$(docker run --name ${name}-test -d \
+                         -e LANG=en_US.UTF-8 -e PARAMETER_THAT_MAY_NEED_ESCAPING="&'\"|< é\\(" ${name} \
+                         ${entrypoint} --commandLineArgValueThatMayNeedEscaping="&'\"|< é\\(" --killDelay=1 --exitCode=0)
+
+  # sleep is required because after docker run returns, the container is up but our server may not quite be yet
+  local exitCode=$(docker wait ${container_id})
+
+  if [ "$exitCode" = '0' ]; then
+    echo "APP TEST PASSED (with entrypoint ${entrypoint})"
+    docker rm -f ${container_id}
+    return 0
+  else
+    echo "APP TEST FAILED (with entrypoint ${entrypoint})"
+    docker logs ${container_id}
+    docker rm -f ${container_id}
+    return -123
+  fi
+}
+
+# ==================================================================================
+
+function test_container() {
+  test_app $1
+}
+
+# ==================================================================================
+
+function test_runtime_image() {
+  local dir=$1
+  local name=$2
+  local builderImage=$3
+
+  docker build ${dir} -t ${name}
+
+  # ----------------------------------------------------------------------------------
+  # Presence of any required tools
+  #  * Issues #171 and #184: unzip is required for Spring Boot devtools support
+  # ----------------------------------------------------------------------------------
+
+  docker run --rm --name ${name}-test-unzip ${builderImage} unzip
+
+  # --------------------------------------------------------------------------------
+  # Gradle Spring Boot WAR  <https://github.com/fabric8io-images/s2i/issues/123>
+  # --------------------------------------------------------------------------------
+
+  s2i build --copy tmp/s2i-java-container/java/examples/spring-gradle ${builderImage} ${name}-spring-gradle --runtime-image ${name}
+
+  test_container "${name}-spring-gradle"
+
+
+  # ----------------------------------------------------------------------------------
+  # Maven
+  # ----------------------------------------------------------------------------------
+
+  s2i build --copy tmp/s2i-java-container/java/examples/maven ${builderImage} ${name}-maven-example --runtime-image ${name}
+
+
+  test_container "${name}-maven-example"
+
+
+  # --------------------------------------------------------------------------------
+  # Gradle
+  # --------------------------------------------------------------------------------
+
+  s2i build --copy tmp/s2i-java-container/java/examples/gradle ${builderImage} ${name}-gradle-example  --runtime-image ${name}
+
+  # TODO https://github.com/fabric8io-images/s2i/issues/150
+  # s2i build --copy java/examples/gradle ${name} ${name}-gradle-example --incremental
+
+  test_container "${name}-gradle-example"
+
+
+  # ----------------------------------------------------------------------------------
+  # Binary
+  # ----------------------------------------------------------------------------------
+
+  mvn -f tmp/s2i-java-container/java/examples/maven/pom.xml clean package
+  mkdir -p tmp/s2i-java-container/java/examples/binary/deployments/
+  cp tmp/s2i-java-container/java/examples/maven/target/*.jar tmp/s2i-java-container/java/examples/binary/deployments/
+
+  s2i build --copy tmp/s2i-java-container/java/examples/binary/ ${builderImage} ${name}-binary-example --runtime-image ${name}
+  rm tmp/s2i-java-container/java/examples/binary/deployments/*
+
+  test_container "${name}-binary-example"
+
+
+  # ----------------------------------------------------------------------------------
+  # Maven Wrapper
+  # ----------------------------------------------------------------------------------
+
+  s2i build --copy tmp/s2i-java-container/java/examples/maven-wrapper ${builderImage} ${name}-maven-wrapper-example --runtime-image ${name}
+
+
+  # ----------------------------------------------------------------------------------
+  # Entrypoint Binary
+  # ----------------------------------------------------------------------------------
+  if [ "$name" =  "s2i-java8-runtime" ]; then
+    curl https://repo.spring.io/release/org/springframework/cloud/spring-cloud-deployer-spi-test-app/1.3.4.RELEASE/spring-cloud-deployer-spi-test-app-1.3.4.RELEASE-exec.jar \
+         -o tmp/s2i-java-container/java/examples/binary/deployments/app.jar
+
+    s2i build --copy tmp/s2i-java-container/java/examples/binary/ ${builderImage} ${name}-entrypoint-binary-example --runtime-image ${name}
+    rm tmp/s2i-java-container/java/examples/binary/deployments/*
+
+    test_entrypoint "${name}-entrypoint-binary-example" "java -jar /opt/run-java/deployments/app.jar"  # works
+    test_entrypoint "${name}-entrypoint-binary-example" /opt/run-java/run-java.sh         # will fail until https://github.com/fabric8io-images/run-java-sh/issues/75 is fixed
+    test_entrypoint "${name}-entrypoint-binary-example" /usr/local/s2i/run                # will fail until https://github.com/fabric8io-images/run-java-sh/issues/75 is fixed
+  fi
+}
+
+# ==================================================================================
+
+function test_tomcat_java8_image() {
+  local dir="tomcat85-java8"
+  local name="tomcat85-java8-runtime"
+
+  docker build ${dir} -t ${name}
+
+  s2i build --copy tmp/s2i-java-container/tomcat/examples/spring-mvc-showcase kubespheredev/tomcat85-java8-centos7 ${name}-spring-mvc --runtime-image ${name}
+
+  local port="8080"
+
+  local container_id=$(docker run --name ${name}-spring-mvc-test -d -p ${port} ${name}-spring-mvc)
+
+  # sleep is required because after docker run returns, the container is up but our server may not quite be yet
+  sleep 10
+  local http_port="$(docker port ${container_id} ${port}|sed 's/0.0.0.0://')"
+  local http_reply=$(curl --silent --show-error http://localhost:$http_port/spring-mvc-showcase/simple)
+  echo $http_reply
+  if [ "$http_reply" =  "Hello world!" ]; then
+    echo "APP TEST PASSED"
+    docker rm -f ${container_id}
+    return 0
+  else
+    echo "APP TEST FAILED"
+    docker logs ${container_id}
+    docker rm -f ${container_id}
+    return -123
+  fi
+}
+
+function test_tomcat_java11_image() {
+  local dir="tomcat85-java11"
+  local name="tomcat85-java11-runtime"
+
+  docker build ${dir} -t ${name}
+
+  s2i build --copy tmp/s2i-java-container/tomcat/examples/springmvc5 kubespheredev/tomcat85-java11-centos7 ${name}-spring-mvc-5 --runtime-image ${name}
+
+  local port="8080"
+
+  local container_id=$(docker run --name ${name}-spring-mvc-5-test -d -p ${port} ${name}-spring-mvc-5)
+
+  # sleep is required because after docker run returns, the container is up but our server may not quite be yet
+  sleep 10
+  local http_port="$(docker port ${container_id} ${port}|sed 's/0.0.0.0://')"
+  local http_reply=$(curl --silent --show-error http://localhost:$http_port/spring-mvc-java11/hello)
+  echo $http_reply
+  if [ "$http_reply" =  "Hello world!" ]; then
+    echo "APP TEST PASSED"
+    docker rm -f ${container_id}
+    return 0
+  else
+    echo "APP TEST FAILED"
+    docker logs ${container_id}
+    docker rm -f ${container_id}
+    return -123
+  fi
+}
+
+# ==================================================================================
+
+mkdir tmp && cd tmp && git clone https://github.com/kubesphere/s2i-java-container
+
+test_runtime_image "java8" "s2i-java8-runtime" "kubespheredev/java-8-centos7"
+test_runtime_image "java11" "s2i-java11-runtime" "kubespheredev/java-11-centos7"
+
+
+test_tomcat_java8_image
+test_tomcat_java11_image
+
+rm -rf tmp
